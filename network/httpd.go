efd511ba7440ec2cfbe97c18d9a71709af17a973
cs8425 <cs8425@gmail.com>
2017-10-21 20:06:13 +0800

    httpd add simple log

--- network/httpd.go
+++ network/httpd.go
@@ -1,19 +1,44 @@
 package main
 
 import (
-    "fmt"
     "net/http"
 	"flag"
+	"log"
 )
 
-var port = flag.String("p", ":4040", "bind port")
-var dir = flag.String("d", "./", "bind dir")
+var verbosity = flag.Int("v", 3, "verbosity")
+var port = flag.String("l", ":4040", "bind port")
+var dir = flag.String("d", "./www", "bind dir")
+
+func reqlog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Vln(3, r.Method, r.URL, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	flag.Parse()
-	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	http.Handle("/", reqlog(http.FileServer(http.Dir(*dir))))
 	err := http.ListenAndServe(*port, nil)
 	if err != nil {
-		fmt.Println(err)
+		Vln(0, err)
+	}
+}
+
+func Vf(level int, format string, v ...interface{}) {
+	if level <= *verbosity {
+		log.Printf(format, v...)
+	}
+}
+func V(level int, v ...interface{}) {
+	if level <= *verbosity {
+		log.Print(v...)
+	}
+}
+func Vln(level int, v ...interface{}) {
+	if level <= *verbosity {
+		log.Println(v...)
 	}
 }
 
