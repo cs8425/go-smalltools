ae291910a2b077cb0af53d08a1630e281b6e0088
cs8425 <cs8425@gmail.com>
2019-11-20 16:06:07 +0800

    add gzip for httpd

--- network/httpd.go
+++ network/httpd.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-    "net/http"
+	"compress/gzip"
+	"net/http"
+	"strings"
 	"flag"
 	"log"
 )
 
-var verbosity = flag.Int("v", 3, "verbosity")
-var port = flag.String("l", ":4040", "bind port")
-var dir = flag.String("d", "./www", "bind dir")
+var (
+	gzipLv = flag.Int("gz", 5, "gzip disable = 0, DefaultCompression = -1, BestSpeed = 1, BestCompression = 9")
+
+	verbosity = flag.Int("v", 3, "verbosity")
+	port = flag.String("l", ":4040", "bind port")
+	dir = flag.String("d", "./www", "bind dir")
+)
 
 func reqlog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		Vln(3, r.Method, r.URL, r.RemoteAddr)
-		next.ServeHTTP(w, r)
+		gzw := TryGzipResponse(w, r)
+		defer gzw.Close()
+		next.ServeHTTP(gzw, r)
 	})
 }
 
@@ -40,5 +48,50 @@ func Vln(level int, v ...interface{}) {
 	if level <= *verbosity {
 		log.Println(v...)
 	}
+}
+
+type GzipResponseWriter struct {
+	http.ResponseWriter
+	gzip *gzip.Writer
+}
+
+func (w *GzipResponseWriter) Write(p []byte) (int, error) {
+	if w.gzip == nil {
+		return w.ResponseWriter.Write(p)
+	}
+
+	return w.gzip.Write(p)
+}
+
+func (w *GzipResponseWriter) Close() (error) {
+	if w.gzip != nil {
+		return w.gzip.Close()
+	}
+	return nil
+}
+
+func CanAcceptsGzip(r *http.Request) (bool) {
+	s := strings.ToLower(r.Header.Get("Accept-Encoding"))
+	for _, ss := range strings.Split(s, ",") {
+		if strings.HasPrefix(ss, "gzip") {
+			return true
+		}
+	}
+	return false
+}
+
+func TryGzipResponse(w http.ResponseWriter, r *http.Request) (*GzipResponseWriter) {
+	if !CanAcceptsGzip(r) || *gzipLv == 0 {
+		return &GzipResponseWriter{w, nil}
+	}
+
+	gw, err := gzip.NewWriterLevel(w, *gzipLv)
+	if err != nil {
+		gw = gzip.NewWriter(w)
+	}
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Del("Content-Length")
+
+	return &GzipResponseWriter{w, gw}
 }
 
