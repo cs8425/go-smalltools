9d71adda27c712f2da317589ebc487ac8828bc7f
cs8425 <cs8425@gmail.com>
2019-12-23 10:09:00 +0800

    add PUT method for using TiddlyWiki

--- network/httpd.go
+++ network/httpd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"compress/gzip"
 	"net/http"
+	"io/ioutil"
 	"strings"
 	"flag"
 	"log"
@@ -11,6 +12,8 @@ import (
 var (
 	gzipLv = flag.Int("gz", 5, "gzip disable = 0, DefaultCompression = -1, BestSpeed = 1, BestCompression = 9")
 
+	file = flag.String("f", "/:index.html;/index.html:index.html", "allow put file")
+
 	verbosity = flag.Int("v", 3, "verbosity")
 	port = flag.String("l", ":4040", "bind port")
 	dir = flag.String("d", "./www", "bind dir")
@@ -18,16 +21,61 @@ var (
 
 func reqlog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		Vln(3, r.Method, r.URL, r.RemoteAddr)
+		Vln(3, r.Method, r.URL, r.RemoteAddr, r.Host)
+		w.Header().Add("Service-Worker-Allowed", "/")
 		gzw := TryGzipResponse(w, r)
 		defer gzw.Close()
 		next.ServeHTTP(gzw, r)
 	})
 }
 
+func wiki(next http.Handler) http.Handler {
+	allowFp := make(map[string]string)
+	urls := strings.Split(*file, ";")
+	for _, s := range urls {
+		parts := strings.SplitAfterN(s, ":", 2)
+		if len(parts) > 1 {
+			allowFp[parts[0]] = parts[1]
+		}
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "HEAD":
+			return
+		case "OPTIONS":
+			w.Header().Add("Allow", "GET, HEAD, PUT, OPTIONS")
+			w.Header().Add("DAV", "1, 2") // hack for WebDAV sync adaptor/saver
+			return
+		case "PUT":
+			fp, ok := allowFp[r.URL.Path]
+			if !ok {
+				Vln(3, "[put]Forbidden", r.Method, r.URL, r.RemoteAddr, r.Host)
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				Vln(3, "[put]read", r.Method, r.URL, r.RemoteAddr, r.Host, err)
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			err = ioutil.WriteFile(fp, b, 0644)
+			if err != nil {
+				Vln(3, "[put]save", r.Method, r.URL, r.RemoteAddr, r.Host, err)
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			return
+		default:
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	flag.Parse()
-	http.Handle("/", reqlog(http.FileServer(http.Dir(*dir))))
+	http.Handle("/", reqlog(wiki(http.FileServer(http.Dir(*dir)))))
 	err := http.ListenAndServe(*port, nil)
 	if err != nil {
 		Vln(0, err)
